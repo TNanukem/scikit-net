6641821796b83dfdfce60eab5ef0fcd8d8c9718d
Tiago Toledo Junior <tiago.nanu@gmail.com>
2021-10-05 08:23:08 -0300

    Added versioning for doumentation

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -2,23 +2,78 @@
 set -x
 
 apt-get update
+
 apt-get -y install python3-pip
 python3 -m pip install Sphinx==4.1.2
-apt-get -y install git rsync build-essential
+apt-get -y install git rsync build-essential python3-stemmer python3-gitpython3-virtualenv python3-setuptools
 python3 -m pip install -r requirements.txt
+python3 -m pip install --upgrade rinohtype pygments
 
 pwd
 ls -lah
 export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
 
-make -C docs clean
-make -C docs html
+# make a new temp dir which will be our GitHub Pages docroot
+docroot=`mktemp -d`
  
+export REPO_NAME="${GITHUB_REPOSITORY##*/}"
+  
+##############
+# BUILD DOCS #
+##############
+
+make -C docs clean
+versions="`git for-each-ref '--format=%(refname:lstrip=-1)' refs/remotes/origin/ | grep -viE '^(HEAD|gh-pages)$'`"
+for current_version in ${versions}; do
+  
+   # make the current language available to conf.py
+   export current_version
+   git checkout ${current_version}
+  
+   echo "INFO: Building sites for ${current_version}"
+  
+   # skip this branch if it doesn't have our docs dir & sphinx config
+   if [ ! -e 'docs/conf.py' ]; then
+      echo -e "\tINFO: Couldn't find 'docs/conf.py' (skipped)"
+      continue
+   fi
+  
+   languages="en `find docs/locales/ -mindepth 1 -maxdepth 1 -type d -exec basename '{}' \;`"
+   for current_language in ${languages}; do
+  
+      # make the current language available to conf.py
+      export current_language
+  
+      ##########
+      # BUILDS #
+      ##########
+      echo "INFO: Building for ${current_language}"
+  
+      # HTML #
+      sphinx-build -b html docs/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
+  
+      # PDF #
+      sphinx-build -b rinoh docs/ docs/_build/rinoh -D language="${current_language}"
+      mkdir -p "${docroot}/${current_language}/${current_version}"
+      cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.pdf"
+  
+      # EPUB #
+      sphinx-build -b epub docs/ docs/_build/epub -D language="${current_language}"
+      mkdir -p "${docroot}/${current_language}/${current_version}"
+      cp "docs/_build/epub/target.epub" "${docroot}/${current_language}/${current_version}/helloWorld-docs_${current_language}_${current_version}.epub"
+  
+      # copy the static assets produced by the above build into our docroot
+      rsync -av "docs/_build/html/" "${docroot}/"
+  
+   done
+  
+done
+ 
+ # return to master branch
+git checkout master
+
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
- 
-docroot=`mktemp -d`
-rsync -av "docs/build/html/" "${docroot}/"
  
 pushd "${docroot}"
 
@@ -28,6 +83,20 @@ git checkout -b gh-pages
  
 touch .nojekyll
  
+ # add redirect from the docroot to our default docs language/version
+cat > index.html <<EOF
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>helloWorld Docs</title>
+      <meta http-equiv = "refresh" content="0; url='/${REPO_NAME}/en/master/'" />
+   </head>
+   <body>
+      <p>Please wait while you're redirected to our <a href="/${REPO_NAME}/en/master/">documentation</a>.</p>
+   </body>
+</html>
+EOF
+
 cat > README.md <<EOF
 # GitHub Pages Cache
  
