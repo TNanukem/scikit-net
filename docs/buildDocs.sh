17b62f572bfb571fc235f88b2a9e151564ac7457
Tiago Toledo Junior <tiago.nanu@gmail.com>
2021-10-07 05:42:54 -0300

    Update buildDocs.sh

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -52,7 +52,7 @@ for current_version in ${versions}; do
     cp "docs/build/epub/target.epub" "${docroot}/${current_version}/helloWorld-docs_${current_version}.epub"
 
     # copy the static assets produced by the above build into our docroot
-    rsync -av "docs/build/html/" "${docroot}/"
+    cp -R "docs/build/html/" "${docroot}/"
 
   
 done
