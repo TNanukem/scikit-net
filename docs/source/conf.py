d93e8968986daedc4ec65b8cf46f10e19cc52620
Tiago Toledo Junior <tiago.nanu@gmail.com>
2021-10-11 10:46:55 -0300

    Versioning tag

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,6 +64,15 @@ html_theme_options = {
             "icon": "fab fa-twitter-square",
         },
     ],
+    "navbar_start": ["navbar-logo"],
+    "navbar_end": ["navbar-icon-links", "version"]
+}
+
+html_context = {
+    "versions_dropdown": {
+        "develop": "develop (latest)",
+        "main": "main (stable)",
+    },
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -71,68 +80,6 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-############################
-# SETUP THE RTD LOWER-LEFT #
-############################
-try:
-   html_context
-except NameError:
-   html_context = dict()
-html_context['display_lower_left'] = True
- 
-if 'REPO_NAME' in os.environ:
-   REPO_NAME = os.environ['REPO_NAME']
-else:
-   REPO_NAME = ''
- 
-# SET CURRENT_VERSION
-from git import Repo
-repo = Repo( search_parent_directories=True )
- 
-if 'current_version' in os.environ:
-   # get the current_version env var set by buildDocs.sh
-   current_version = os.environ['current_version']
-else:
-   # the user is probably doing `make html`
-   # set this build's current version by looking at the branch
-   current_version = repo.active_branch.name
- 
-# tell the theme which version we're currently on ('current_version' affects
-# the lower-left rtd menu and 'version' affects the logo-area version)
-html_context['current_version'] = current_version
-html_context['version'] = current_version
- 
-# POPULATE LINKS TO OTHER VERSIONS
-html_context['versions'] = list()
- 
-versions = [branch.name for branch in repo.branches]
-for version in versions:
-   html_context['versions'].append((version, '/' + REPO_NAME + '/'  + version + '/'))
- 
-# POPULATE LINKS TO OTHER FORMATS/DOWNLOADS
- 
-# settings for creating PDF with rinoh
-rinoh_documents = [(
- master_doc,
- 'target',
- project+ ' Documentation',
- '© ' +copyright,
-)]
-today_fmt = "%B %d, %Y"
- 
-# settings for EPUB
-epub_basename = 'target'
- 
-html_context['downloads'] = list()
-html_context['downloads'].append( ('pdf', '/' +REPO_NAME+ '/' +current_version+ '/' +project+ '-docs_' + '_' +current_version+ '.pdf') )
- 
-html_context['downloads'].append( ('epub', '/' +REPO_NAME+ '/' +current_version+ '/' +project+ '-docs_' + '_' +current_version+ '.epub') )
 
-##########################
-# "EDIT ON GITHUB" LINKS #
-##########################
- 
-html_context['display_github'] = True
-html_context['github_user'] = 'maltfield'
-html_context['github_repo'] = 'rtd-github-pages'
-html_context['github_version'] = 'master/docs/'
+def setup(app):
+    app.add_css_file("custom.css")
